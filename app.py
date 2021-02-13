5e74ee042d672292c0436c1b1cc574b4dbfbd1dd
Kushagra Tandon <kushagra.tandon.124@gmail.com>
2021-02-14 00:29:57 +0530

    TM Four_Two

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from processing import process,process2,recipients
+from processing import process, process2, recipients
 import json
 from functools import wraps
-from flask import Flask, request ,make_response
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
@@ -14,27 +15,29 @@ def predict():
     if request.method == 'GET':
         return 'Send a POST request here'
     jsonfile = request.json
-    #print(type(jsonfile))
-    #print(jsonfile)
+    # print(type(jsonfile))
+    # print(jsonfile)
     result = process(jsonfile)
     res = {"code": 200, "isUploaded": True, "allFlags": result}
     return res
 
-@app.route('/api',methods=['GET','POST'])
+
+@app.route('/api', methods=['GET', 'POST'])
 def predict2():
-    if request.method=='GET':
+    if request.method == 'GET':
         return 'Send An API Post Request Here'
     jsonfile = request.json
     print(type(jsonfile))
     jsonfile = json.loads(jsonfile)
     print(type(jsonfile))
-    result=process2(jsonfile)
+    result = process2(jsonfile)
     res = {"code": 200, "isUploaded": True, "allFlags": result}
     return res
 
-@app.route('/email',methods=['GET','POST'])
+
+@app.route('/email', methods=['GET', 'POST'])
 def email_sys():
-    if request.method=='GET':
+    if request.method == 'GET':
         return 'Send An Email Request Here'
     jsonfile = request.json
     #jsonfile = json.loads(jsonfile)
@@ -43,6 +46,7 @@ def email_sys():
     # print(type(jsonfile))
     recipients(jsonfile)
     return "Hi Kushagra Tandon/Bajpai Hero's"
+
 
 if __name__ == '__main__':
     app.run()
